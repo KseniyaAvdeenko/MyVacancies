5ff6d0100806d8c48b80ad015dbb0cc4943eac89
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2024-11-11 23:58:56 +0300

    fix for build

--- service/AuthService.ts
+++ service/AuthService.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 class AuthService {
     async login(user: {name: string, password: string}){
-       return await axios.post(process.env.NEXT_PUBLIC_BASE_URL + 'api/auth/login', JSON.stringify(user))
+       return await axios.post('api/auth/login', JSON.stringify(user))
     }
     async logout(){
-       return await axios.post(process.env.NEXT_PUBLIC_BASE_URL + 'api/auth/logout')
+       return await axios.post('api/auth/logout')
     }
 }
 export default AuthService;
--- service/VacancyService.ts
+++ service/VacancyService.ts
@@ -3,20 +3,20 @@ import {IVacancy, IVacancyStatus, Status} from "@/interface/IVacancy";
 
 class VacancyService {
     async getVacancies() {
-        const response = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + 'api/vacancies')
+        const response = await axios.get( 'api/vacancies')
         const data: IVacancy[] = response.data;
         return data;
     }
 
     async getStatus() {
-        const response = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + 'api/status')
+        const response = await axios.get('api/status')
         const data: IVacancyStatus[] = response.data
         return data;
     }
 
     async createVacancy(data: IVacancy) {
         try {
-            return await axios.post<IVacancy>(process.env.NEXT_PUBLIC_BASE_URL + 'api/vacancies', JSON.stringify(data)).then(response => response.data);
+            return await axios.post<IVacancy>('api/vacancies', JSON.stringify(data)).then(response => response.data);
         } catch (e) {
             console.log(e)
             return null
@@ -25,7 +25,7 @@ class VacancyService {
 
     async updateVacancy(id: number, data: IVacancy) {
         try {
-            return await axios.put<IVacancy>(process.env.NEXT_PUBLIC_BASE_URL + 'api/vacancies/' + id, JSON.stringify(data)).then(response => response.data);
+            return await axios.put<IVacancy>('api/vacancies/' + id, JSON.stringify(data)).then(response => response.data);
         } catch (e) {
             console.log(e)
             return null
